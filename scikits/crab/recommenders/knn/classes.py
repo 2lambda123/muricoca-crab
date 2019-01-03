235eabfae13357f1ebc4b92f7e6139899c07062b
MostaSchoolOfAI <44090479+MostaSchoolOfAI@users.noreply.github.com>
2019-01-03 18:40:39 +0100

    Update classes.py

--- scikits/crab/recommenders/knn/classes.py
+++ scikits/crab/recommenders/knn/classes.py
@@ -598,7 +598,7 @@ class UserBasedRecommender(UserRecommender):
         '''
 
         #self._set_params(**params)
-        Super(BaseEstimator, self)._set_params(**params)
+        super(BaseEstimator, self)._set_params(**params)
 
         candidate_items = self.all_other_items(user_id, **params)
 
